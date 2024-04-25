4e5d57f47b4b5ee7859b37cb9080123f0b45c809
victoriaschwin <vicky.schwint95@gmail.com>
2024-04-25 20:34:24 +0200

    getUserById and testing positive and negative response

--- src/main/java/com/example/demo/controllers/ReservationController.java
+++ src/main/java/com/example/demo/controllers/ReservationController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controllers;
 
+import com.example.demo.repositories.ReservationRepository;
+import com.example.demo.services.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReservationController {
     @Autowired
-    private ReservationController reservationController;
+    private ReservationRepository reservationRepository;
+    @Autowired
+    private ReservationService reservationService;
 }
